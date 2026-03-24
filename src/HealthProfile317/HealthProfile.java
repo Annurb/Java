ca18bc1476ef477dd18b6eef0658ec9239045c18
Annurb <brunna.raphaela1@gmail.com>
2026-03-24 14:26:30 -0300

    atualiza funcinalidade

--- src/HealthProfile317/HealthProfile.java
+++ src/HealthProfile317/HealthProfile.java
@@ -1,4 +1,5 @@
 package HealthProfile317;
+import java.time.LocalDateTime;
 
 public class HealthProfile{
     private String firstName;
@@ -7,8 +8,13 @@ public class HealthProfile{
     private int day;
     private int month;
     private int year;
-    private int height;
+    private double height;
     private double weight;
+    private int age;
+    private int maxFrequency;
+    private int targetFrequency1;
+    private int targetFrequency2;
+    private double imc;
 
     public static void Main(String[] args){
 
@@ -58,13 +64,50 @@ public class HealthProfile{
     public int getYear(){
         return year;
     }
-    public int getHeight(){
+    public double getHeight(){
         return height;
     }
     public double weight(){
         return weight;
     }
 
+    // Calculo da idade
+    public int getAge(){
+        LocalDateTime now = LocalDateTime.now();
+        int age1Month;
+        int age1Year;
 
-
+        int reservedDay = now.getDayOfMonth() - day;
+        if reservedDay < 1{
+            age1Month = now.getMonthValue() - 1;
+        }
+        else{
+            age1Month= now.getMonthValue();
+        }
+        int reservedMonth = age1Month - month;
+        if(reservedMonth < 1){
+            age1Year = now.getYear()-1;
+        }
+        else{
+            age1Year = now.getYear();
+        }
+        age = age1Year - year;
+        return age;
+    }
+    public int getMaxFrequency(){
+        maxFrequency = 220 - age;
+        return maxFrequency;
+    }
+    public int getTargetFrequency1(){
+        targetFrequency1 = maxFrequency/2;
+        return targetFrequency1;
+    }
+    public int getTargetFrequency2() {
+        targetFrequency2 = (maxFrequency / 100) *85;
+        return targetFrequency2;
+    }
+    public double getImc(){
+        imc = weight/(height**2)
+        return imc;
+    }
 }
