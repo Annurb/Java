83d5f33aa5bd2e0b3bee807159b97d7bef6290cb
Annurb <brunna.raphaela1@gmail.com>
2026-05-28 09:48:34 -0300

    Adiciona exercicio 10.13

---
+++ src/HierarquiaShape1013/main.java
@@ -0,0 +1,35 @@
+package HierarquiaShape1013;
+
+public class main {
+}
+
+abstract class Shape{
+
+}
+// Shape
+class TwoDimensionalShape extends Shape{
+
+}
+class ThreeDimensionalShape extends Shape{
+
+}
+//TwoDimentsionalShape
+class Circle extends TwoDimensionalShape{
+
+}
+class Square extends TwoDimensionalShape{
+
+}
+class Triangle extends TwoDimensionalShape{
+
+}
+// ThreeDimensionalSape
+class Sphere extends ThreeDimensionalShape{
+
+}
+class Cube extends ThreeDimensionalShape{
+
+}
+class Tetrahedron extends ThreeDimensionalShape{
+
+}
